a066dbb54685e6a32e71eb928cff7ce8da569736
sandeepsomavarapu <42529810+sandeepsomavarapu@users.noreply.github.com>
2025-11-06 13:03:13 +0530

    Login

--- Form.js
+++ Form.js
@@ -1,37 +1,39 @@
 import React from 'react'
 
-const Register = () => {
- const [username, setUsername] = React.useState("");
-   const [password, setPassword] = React.useState("");
-   const [firstname, setFirstname] = React.useState("");
+const Login = () => {
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [loginStatus, setLoginStatus] = React.useState(false);
 
-   const handleSubmit = (e) => {
-     e.preventDefault();
-     localStorage.setItem("username", username);
-     localStorage.setItem("password", password);
-      console.log("Registration Successful");
-   }
- 
-   return (
-     <div>
-       <form className='FormGroup m-3 p-3 border border-dark rounded' onSubmit={handleSubmit} >
-         <h2>Registration Page</h2>
-         <div>
-           <label>Username: </label>
-           <input className='form-control' value={username} onChange={(e) => setUsername(e.target.value)} type="text" />
-         </div>
-         <div>
-           <label>Password: </label>
-           <input className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
-         </div>
-         <div>
-           <label>Firstname: </label>
-           <input className='form-control' value={firstname} onChange={(e) => setFirstname(e.target.value)} type="text" />
-         </div>
-         <button className='btn btn-primary' type="submit">Register</button>
-       </form>
-     </div>
-   )
- }
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const uname = localStorage.getItem("username");
+  const pass = localStorage.getItem("password");
 
-export default Register
+  if (username === uname && password === pass) {
+    alert("Login Success");
+    localStorage.setItem("token", "authenticated"); 
+    window.location.href = "/home"; 
+  } else {
+    alert("Login Failed");
+  }
+};
+  return (
+    <div>
+      <form className='FormGroup m-3 p-3 border border-dark rounded' onSubmit={handleSubmit} >
+        <h2>Login Page</h2>
+        <div>
+          <label>Username: </label>
+          <input className='form-control' value={username} onChange={(e) => setUsername(e.target.value)} type="text" />
+        </div>
+        <div>
+          <label>Password: </label>
+          <input className='form-control' value={password} onChange={(e) => setPassword(e.target.value)} type="password" />
+        </div>
+        <button className='btn btn-primary' type="submit">Login</button>
+      </form>
+    </div>
+  )
+}
+
+export default Login
